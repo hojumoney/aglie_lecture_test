9e5d1a197b6427ac12fef00d12a6853b03298f7c
hojumoney <hojun0090@naver.com>
2024-08-22 10:53:58 +0900

    commit

---
+++ frontend/src/components/DeliveryCommand.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-card outlined>
+        <v-card-title>
+            Delivery
+        </v-card-title>
+
+        <v-card-text>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="primary"
+                    text
+                    @click="delivery"
+            >
+                Delivery
+            </v-btn>
+            
+            <v-btn
+                    color="primary"
+                    text
+                    @click="close"
+            >
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+<script>
+   
+    export default {
+        name: 'DeliveryCommand',
+        components:{},
+        props: {},
+        data: () => ({
+            editMode: true,
+            value: {},
+        }),
+        created() {
+        },
+        watch: {
+        },
+        methods: {
+            delivery() {
+                this.$emit('delivery', this.value);
+            },
+            close() {
+                this.$emit('closeDialog');
+            },
+            change() {
+                this.$emit('input', this.value);
+            },
+        }
+    }
+</script>
+
